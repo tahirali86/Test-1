0741990775304802ad4bbd687c0f8af61462c0d2
tahir.ali3 <tahirali86@gmail.com>
2022-02-09 14:07:51 +0500

    first Commit

---
+++ src/Github/Main.java
@@ -0,0 +1,11 @@
+package Github;
+
+public class Main {
+
+    public static void main(String[] args) {
+        char Word;
+
+        for(Word = 'A'; Word <= 'Z'; ++Word)
+            System.out.print(Word + " ");
+    }
+}
